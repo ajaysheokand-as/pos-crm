d85e5b468eda30b79bad941b27ac33895fe1dca3
abhishekdhull <46604435+abhishekdhull21@users.noreply.github.com>
2025-07-16 18:20:38 +0530

    commented the headers

--- application/controllers/Api/ApiController.php
+++ application/controllers/Api/ApiController.php
@@ -25,13 +25,13 @@ class ApiController extends CI_Controller
 
         // Check if the incoming Origin is in the allowed list
         if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
-            header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
-            header("Access-Control-Allow-Credentials: true");
+//            header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+//            header("Access-Control-Allow-Credentials: true");
         }
 
         // Always set these
-        header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+//        header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+//        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 
         // Preflight handling
         if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
